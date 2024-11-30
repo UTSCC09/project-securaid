1574f92c5a9221eb110a2272fdc37a7ab321f1d7
Dhruvin Patel <pateldhruvin2503@gmail.com>
2024-11-30 12:47:08 -0500

    Added credits

---
+++ client/app/credits/page.tsx
@@ -0,0 +1,41 @@
+// pages/credits.tsx
+"use client";
+
+import Link from "next/link";
+import "./page.css";
+
+function Credits() {
+  return (
+    <div className="credits-container">
+      <h1>Credits</h1>
+      <p>Here are the resources that helped build this project:</p>
+      <ul>
+        <li>
+          <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
+            React.js Documentation
+          </a>
+        </li>
+        <li>
+          <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">
+            Next.js Documentation
+          </a>
+        </li>
+        <li>
+          <a href="https://notistack.com/" target="_blank" rel="noopener noreferrer">
+            Notistack for Notifications
+          </a>
+        </li>
+        <li>
+          <a href="https://magicui.design/" target="_blank" rel="noopener noreferrer">
+            magicui.design for Globe Visualization
+          </a>
+        </li>
+      </ul>
+      <p>
+        Back to the <Link href="/">Homepage</Link>.
+      </p>
+    </div>
+  );
+}
+
+export default Credits;
