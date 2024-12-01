2276e00f8de3c73ab16c041452f7234c5e2e7cde
Dhruvin Patel <pateldhruvin2503@gmail.com>
2024-11-30 23:37:41 -0500

    Fixing Google auth

--- server/index.js
+++ server/index.js
@@ -68,7 +68,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://securaid.mywire.org/auth/google/callback",
+      callbackURL: "https://securaid-backend.mywire.org/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
